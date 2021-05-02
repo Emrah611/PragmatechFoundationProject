7e06858aecd20b77d88c2825295a541cef31cd42
Emrah611 <bey_emrah@mail.ru>
2021-05-02 18:00:52 +0300

    commit

--- LAYIHELER/LAYIHE/script.js
+++ LAYIHELER/LAYIHE/script.js
@@ -1,23 +1,38 @@
-const animationItems = document.querySelectorAll('._anim');
-const animationOnScroll = () => {
-    if (animationItems.length > 0) {
-        for (let i = 0; i < animationItems.length; i++) {
-            // Animasiya ne zaman baslasin
-            const animationStart = 4;
-            // ekranin olcusu elementin olsunun 1/4 oldugda animasi aktivlessin
-            let animationItemPoint = window.innerHeight - animationItems[i].offsetHeight / animationStart;
-            // Eger scroll deyeri > sehifenin en basindan elemente geder olan mesafe - elementin 1/4 geder
-            if ((pageYOffset > (animationItems[i].offsetTop - animationItemPoint)) && (pageYOffset < (animationItems[i].offsetTop + animationItems[i].offsetHeight))) {
-                animationItems[i].classList.add('_active');
-            } else {
-                // Animasiyani  tekrarlanmamasi ucun sert
-                if (!animationItems[i].classList.contains('_noRepeat')) {
-                    animationItems[i].classList.remove('_active');
-                }
-            }
-        }
+window.onscroll = function() { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
     }
 }
-window.addEventListener('scroll', animationOnScroll);
-// Sehife acilanda il section animasiyanin aktivlesmesi
-animationOnScroll();
+
+
+// let images = document.querySelectorAll('.mySlides');
+// let start = 0;
+// images[start].style.display = 'block';
+
+// document.querySelector('.next').addEventListener('click', () => {
+//     if (start == images.length - 1) {
+//         start = -1;
+//     };
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].style.display = 'none';
+//     }
+//     images[++start].style.display = 'block';
+// })
+
+// document.querySelector('.prev').addEventListener('click', () => {
+//     start -= 1;
+//     if (start < 0) {
+//         start = images.length - 1;
+//     }
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].style.display = 'none';
+//     }
+//     images[start].style.display = 'block';
+// })
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # PragmatechFoundationProject
 
-https://colorlib.com/preview/#mediplus
+Project link [https://colorlib.com/preview/#mediplus]
+
+[01.04.21] started "Layihe" 
+
+[03.04.21] Layihenin ilk sehifesi hazir oldu [img,border-radius,col-mentiqi,animation]
+
+[07.04.21] Layihenin 3 sehifesi hazirdi [img,form,input,label]
+
+[15.04.21] Layihenin 5 sehifesi hazirdi [position-relative,position-absalute,Yonlendirme(href'#')]
+
+[17.04.21] SON [position-fixed]
+
+[25.04.21] started "Layihe2"
+
+[30.04.21] SON [position-fixed]
