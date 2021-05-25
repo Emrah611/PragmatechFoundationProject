47b67942b54adf695565e4ef35aedea6b1d0d9de
Emrah611 <bey_emrah@mail.ru>
2021-05-25 11:22:28 +0300

    commit

--- Flask/app.py
+++ Flask/app.py
@@ -3,11 +3,11 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/html1')
-def Home():
+def home():
     return render_template('html1/index1.html')
 
 @app.route('/html2')
-def Func():
+def func():
     return render_template('html2/index2.html')
 
 if __name__ == '__main__':
