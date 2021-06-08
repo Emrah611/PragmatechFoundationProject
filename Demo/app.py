8c4614f7d75f8d87d149e9952c79b0bc7681ce67
Emrah611 <bey_emrah@mail.ru>
2021-06-08 11:27:29 +0300

    commit

--- Demo/app.py
+++ Demo/app.py
@@ -5,11 +5,11 @@ from os.path import join, dirname, realpath, os
 from sqlalchemy.orm import backref
 from werkzeug.utils import secure_filename
 from datetime import datetime
-from flask_wtf import FlaskForm
-from wtfforms.fields.simple import FileField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from wtforms import Form, BooleanField, StringField, PasswordField, SubmitField, validators
-from pavshop.models import User
+# from flask_wtf import FlaskForm
+# from wtfforms.fields.simple import FileField
+# from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+# from wtforms import Form, BooleanField, StringField, PasswordField, SubmitField, validators
+# from pavshop.models import User
 UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads/')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -18,7 +18,6 @@ app.config['SECRET KEY'] = 'you-will-never-guess'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 # from image_upload import save_picture
-
 
 @app.route("/admin-patient-list")
 def patient_list():
@@ -76,7 +75,40 @@ def patient_delete(id):
     db.session.commit()
     return redirect(url_for('patient_list'))
 
+@app.route("/admin-profession", methods=['GET', "POST"])
+def profession():
+    professions = Profession.query.all()
+    if request.method == 'POST':
+        profession = Profession(
+        specialty = request.form['specialty'],
+        description = request.form['description']
+        )
+        db.session.add(profession)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("admin/profession.html",professions=professions)
 
+@app.route("/admin-profession-edit/<int:id>", methods=['GET', "POST"])
+def profession_edit(id):
+    profession = Profession.query.get_or_404(id)
+    if request.method == 'POST':
+        profession.specialty = request.form['specialty']
+        profession.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('profession'))
+    return render_template('admin/profession-edit.html')
+
+@app.route("/admin-profession-delete/<int:id>")
+def profession_delete(id):
+    profession = Profession.query.get_or_404(id)
+    db.session.delete(profession)
+    db.session.commit()
+    return redirect(url_for('profession_list'))
+
+@app.route("/admin-profession-list")
+def profession_list():
+    professions = Profession.query.all()
+    return render_template("admin/profession-list.html",professions=professions)
 
 class Patient(db.Model):
 
@@ -102,11 +134,20 @@ class PatientCategory(db.Model):
     def __repr__(self):
         return f'Category {self.name}'
 
+class Profession(db.Model):
+    __tablename__ = 'profession'
+    id = db.Column(db.Integer, primary_key=True)
+    specialty = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.String(30),nullable=False)
+
+    def __repr__(self):
+        return f'Profession {self.specialty}'
 
 #USER 
 @app.route('/')
 def home():
-    return render_template('med1.html')
+    professions = Profession.query.all()
+    return render_template('med1.html',professions=professions)
 
 @app.route('/medical')
 def medical():
@@ -137,35 +178,3 @@ def randevu():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.route("/admin")
-# def admin():
-#     return render_template("admin/admin.html")
-
-# @app.route('/admin/patient-edit') 
-# def patient_edit():
-#     patients = patient.query.all()
-#     return render_template('admin/patient-edit.html', patients=patients)
-
-
